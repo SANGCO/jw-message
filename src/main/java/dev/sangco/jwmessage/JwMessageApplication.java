af6850aa624673c65a93e446e05ba7a0e07b2d95
sangco <nsg1213@gmail.com>
2018-05-25 15:14:42 +0900

    Initial commit

---
+++ src/main/java/dev/sangco/jwmessage/JwMessageApplication.java
@@ -0,0 +1,12 @@
+package dev.sangco.jwmessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwMessageApplication.class, args);
+	}
+}
